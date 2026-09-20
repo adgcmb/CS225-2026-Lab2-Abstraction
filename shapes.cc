26e47cfa447a6a20d3bde32f09782da59fbe2d45
adgcmb <a.delgado@digipen.edu>
2026-09-20 16:32:38 +0200

    lab done

--- shapes.cc
+++ shapes.cc
@@ -9,7 +9,14 @@ Circle::Circle(int r){
 }
 
 Triangle::Triangle(int b, int h){
-    base = b; heigth = h;
+    base = b; 
+    heigth = h;
+}
+
+Rectangle::Rectangle(int b,int h)
+{
+    base = b;
+    heigth = h;
 }
 
 float Circle::CalculateArea() {
@@ -20,10 +27,50 @@ float Triangle::CalculateArea() {
     return base * heigth/2; 
 }
 
+float Rectangle::CalculateArea()
+{
+    return base * heigth;
+}
+
 void Circle::Draw() {
-    printf("Circle\n");
+    printf("Draw Circle\n");
 }
 
 void Triangle::Draw() {
-    printf("Draw\n");
+    printf("Draw Triangle\n");
+}
+
+void Rectangle::Draw()
+{
+    printf("Draw Rectangle\n");
+}
+
+float Circle::CalculatePerimeter()
+{
+    return 2 * 3.141516f * radius;
+}
+
+float Triangle::CalculatePerimeter()
+{
+    float hypotenuse = sqrt(base * base + heigth * heigth);
+    return base + heigth + hypotenuse;
+}
+
+float Rectangle::CalculatePerimeter()
+{
+    return 2 * (base + heigth);
+}
+
+void Circle::Scale(float scaFactor) {
+    radius = radius * scaFactor;
+}
+
+void Triangle::Scale(float scaFactor) {
+    base = base * scaFactor;
+    heigth = heigth * scaFactor;
+}
+
+void Rectangle::Scale(float scaFactor) {
+    base = base * scaFactor;
+    heigth = heigth * scaFactor;
 }
--- shapes.hh
+++ shapes.hh
@@ -4,18 +4,26 @@
 #include <iostream>
 #include <math.h>
 
+class IScale
+{
+    public:
+        virtual Scale(float scaFactor) = 0;
+};
+
 class IDrawable
 {
     public:
         virtual void Draw() = 0;
 };
 
-class Shape : public IDrawable {
+class Shape : public IDrawable, public IScale {
     protected:
         const char* color;
     public:
         virtual void setColor(const char *c);
-        virtual float CalculateArea() = 0;
+        virtual float CalculateArea() = 0
+        virtual float CalculatePerimeter() = 0;
+
 };
 
 class Circle : public Shape{
@@ -24,6 +32,8 @@ class Circle : public Shape{
         Circle(int r);
         float CalculateArea() override;
         void Draw() override;
+        float CalculatePerimeter() override;
+        void Scale(float scaFactor) override;
 };
 
 class Triangle : public Shape {
@@ -32,6 +42,18 @@ class Triangle : public Shape {
         Triangle(int b,int h);        
         float CalculateArea() override;
         void Draw() override;
+        float CalculatePerimeter() override;
+        void Scale(float scaFactor) override;
+};
+
+class Rectangle : public Shape {
+    int base, heigth;
+    public:
+        Rectangle(int b,int h);        
+        float CalculateArea() override;
+        void Draw() override;
+        float CalculatePerimeter() override;
+        void Scale(float scaFactor) override;
 };
 
 #endif
